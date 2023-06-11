a4c37a1d51246ea33d4e36f3961198ce405d38b0
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-11 10:31:29 +0700

    update test to use createAndroidComposeRule

--- app/src/androidTest/java/com/dicoding/compose/testing/mct/ui/CalculatorAppKtTest.kt
+++ app/src/androidTest/java/com/dicoding/compose/testing/mct/ui/CalculatorAppKtTest.kt
@@ -1,6 +1,8 @@
 package com.dicoding.compose.testing.mct.ui
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasNoClickAction
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
@@ -8,15 +10,16 @@ import com.dicoding.compose.testing.mct.ui.theme.MyComposeTestingTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import com.dicoding.compose.testing.mct.R
 
 class CalculatorAppKtTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeAndroidRule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun setup() {
-        composeTestRule.setContent {
+        composeAndroidRule.setContent {
             MyComposeTestingTheme {
                 CalculatorApp()
             }
@@ -24,11 +27,20 @@ class CalculatorAppKtTest {
     }
 
     @Test
-    fun calculate_area_or_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+    fun calculate_area_of_rectangle_correct() {
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.enter_length)).performTextInput("3")
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.enter_width)).performTextInput("4")
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.count)).performClick()
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.count), useUnmergedTree = true).assertHasNoClickAction()
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.result, 12.0)).assertExists()
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.enter_length)).performTextInput("..3")
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.enter_width)).performTextInput("4")
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.count)).performClick()
+        composeAndroidRule.onNodeWithText(composeAndroidRule.activity.getString(R.string.result, 0.0)).assertExists()
     }
 
 }
